8ca6cffa753da2bd8b79868588e24882188eb04e
Oskari Palmgren <jokpal@utu.fi>
2023-12-05 12:23:37 +0200

    Initial menu

--- frontend/components/Menu.jsx
+++ frontend/components/Menu.jsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom'
+
 const Menu = () => {
+
+    const paddingR = {
+		paddingRight: 5
+    }
+
+    const background = {
+		background: '#F0F8FF',
+        padding: 10
+	}
+
     return (
-        <div>
-            This is menu component
+
+        <div style={background}>
+            <Link style={paddingR} to="/">Home</Link>
+            <Link style={paddingR} to="/champions">Champions</Link>
+            <Link style={paddingR} to="/items">Items</Link>
+            <Link to="/leaderboards">Leaderboards</Link>
         </div>
     )
 }
