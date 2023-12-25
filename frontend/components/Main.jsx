2af22fc6378871c3255c2faf74f77b46a641a681
Oskari Palmgren <jokpal@utu.fi>
2023-12-25 23:50:45 +0200

    Frontend to show summoner information

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+import summonerService from "../src/services/summoners";
+
 const Main = () => {
-  return <div>Main page</div>;
+  const [summonerData, setSummonerData] = useState(null);
+
+  // Let's just do this initially, always load the preset summoner data when we get to main view.
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await summonerService.getSummonerData();
+      console.log(response);
+      setSummonerData(response);
+    };
+    fetchData();
+  }, []);
+
+  if (!summonerData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Main Page</h1>
+      {summonerData && (
+        <div>
+          <h2>Name: {summonerData.name}</h2>
+          <p>Level: {summonerData.summonerLevel}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Main;
