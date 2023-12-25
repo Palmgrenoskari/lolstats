bf65c7835deec9f7ab10bd2ace59c817fc14af37
Oskari Palmgren <jokpal@utu.fi>
2023-12-26 00:08:43 +0200

    Add profile picture to summoner search

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -3,6 +3,7 @@ import summonerService from "../src/services/summoners";
 
 const Main = () => {
   const [summonerData, setSummonerData] = useState(null);
+  const [summoner, setSummoner] = useState("");
 
   // Let's just do this initially, always load the preset summoner data when we get to main view.
   useEffect(() => {
@@ -20,13 +21,31 @@ const Main = () => {
 
   return (
     <div>
-      <h1>Main Page</h1>
-      {summonerData && (
-        <div>
-          <h2>Name: {summonerData.name}</h2>
-          <p>Level: {summonerData.summonerLevel}</p>
-        </div>
-      )}
+      <h2>Search Summoner (Not working yet..)</h2>
+      <form>
+        <input
+          type="text"
+          name="summoner"
+          value={summoner}
+          placeholder="Insert Summoner Name"
+          onChange={({ target }) => setSummoner(target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {summonerData && (
+          <div>
+            <h3>Name: {summonerData.name}</h3>
+            <p>Level: {summonerData.summonerLevel}</p>
+            <img
+              src={`https://ddragon.leagueoflegends.com/cdn/13.24.1/img/profileicon/${summonerData.profileIconId}.png`}
+              alt="Profile icon"
+              width="300"
+              height="300"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
