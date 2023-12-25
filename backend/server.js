ddd38cea5c910e6c3261e3d2d58db97502cec6c5
Oskari Palmgren <jokpal@utu.fi>
2023-12-25 23:02:58 +0200

    backend summoner name fetching

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
-import express from 'express'
+import express from "express";
+import config from "./utils/config";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+const API_KEY = config.API_KEY;
+// Temporary
+const player_name = "Nahg";
+
+const API_URL = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${player_name}?api_key=${API_KEY}`;
+
+app.get("/api/summoner", async (req, res) => {
+  try {
+    const response = await fetch(API_URL);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data" });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Our awesome LoL App is listering to http://localhost:${port}`)
-})
+  console.log(`Our awesome LoL App is listering to http://localhost:${port}`);
+});
---
+++ backend/utils/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+API_KEY = LEAGUE_API_KEY;
+
+module.exports = {
+  API_KEY,
+};
