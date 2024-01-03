a2bd75cd2c427fd3e426e197fb3d505baa4295a5
Oskari Palmgren <jokpal@utu.fi>
2024-01-03 14:17:33 +0200

    Extract HTML from item desc json object, add img

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -8,11 +8,17 @@ const Item = ({ item }) => {
   return (
     <div>
       <h2>{item.name}</h2>
-      <p>{item.plaintext}</p>
 
-      <p>
-        <b>Need to figure out how to parse this:</b> {item.description}
-      </p>
+      <img
+        src={`https://ddragon.leagueoflegends.com/cdn/13.24.1/img/item/${item.image.full}`}
+        alt={item.name}
+        width={150}
+        height={150}
+      />
+
+      <p>{item.plaintext}</p>
+      {/* Not quite sure how safe this solution is in the longterm but at least */}
+      <div dangerouslySetInnerHTML={{ __html: item.description }} />
     </div>
   );
 };
